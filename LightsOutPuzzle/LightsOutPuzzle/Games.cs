edb3f6b247b265fe8cbc21565e9d6dc7d9aa122c
Daniel Sandberg <elicit.daniel@outlook.com>
2018-04-18 16:51:58 +0200

    Initial commit

---
+++ LightsOutPuzzle/LightsOutPuzzle/Games.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LightsOutPuzzle
+{
+    public class Games
+    {
+        private static Random _random = new Random();
+
+        public static bool[,] RandomGame()
+        {
+            var gameField = new bool[5, 5];
+            for(int row = 0; row < 5;)
+            {
+                for(int col = 0; col< 5;)
+                {
+                    gameField[row, col] = _random.Next(0, 100) > 50;
+                }
+            }
+            return gameField;
+        }
+    }
+}
