7fa5d578b198f9398358c95090c6ae3bd45d230a
Daniel Sandberg <elicit.daniel@outlook.com>
2018-04-24 21:10:41 +0200

    Added Ooui

---
+++ LightsOutPuzzle.Ooui/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ooui.AspNetCore;
+using Xamarin.Forms;
+
+namespace LightsOutPuzzle.Ooui.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var page = new MainPage();
+            var element = page.GetOouiElement();
+            return new ElementResult(element, "Lights out puzzle");
+        }
+    }
+}
