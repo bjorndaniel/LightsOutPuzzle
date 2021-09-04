1b3510246c5b47eb2c41460416e0e4a5ce47fd53
Daniel Sandberg <daniel.sandberg@elicit.se>
2021-09-04 21:27:46 +0200

    Added images and drawing them

--- LightsOutPuzzle.Jetpack/app/src/main/java/com/example/lightsoutpuzzlejetpack/MainActivity.kt
+++ LightsOutPuzzle.Jetpack/app/src/main/java/com/example/lightsoutpuzzlejetpack/MainActivity.kt
@@ -4,32 +4,25 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lightsoutpuzzlejetpack.R.drawable.*
-import com.example.lightsoutpuzzlejetpack.ui.theme.LightsOutPuzzleJetpackTheme
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.fontResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
 import java.util.*
@@ -115,19 +108,76 @@ fun MainPage(){
 @ExperimentalFoundationApi
 @Composable
 fun GameGrid(){
-    val list = (1..25).map { it.toString() }
+    val list = (0..24).map { it }
 
     LazyVerticalGrid(
-        cells = GridCells.Fixed(5),
-        content = {
-            items(list.size) { index ->
-               FlipCard(getColor())
-            }
+        cells = GridCells.Fixed(5)
+    ) {
+        items(list) { item ->
+            var resourceId = getResourceId(item)
+            var img = Image(
+                ImageBitmap.imageResource(id = resourceId),
+                contentDescription = "",
+                modifier = Modifier.height(60.dp).width(60.dp),
+                alignment = Alignment.Center,
+            )
+            FlipCard(img)
         }
-    )
+    }
 }
+
+fun getResourceId(item:Int): Int {
+    var col = item%5
+    var row = item/5
+    when (row) {
+        0 -> when (col) {
+            0 -> return R.drawable.row_1_col_1
+            1 -> return R.drawable.row_1_col_2
+            2 -> return R.drawable.row_1_col_3
+            3 -> return R.drawable.row_1_col_4
+            else ->
+                return R.drawable.row_1_col_5
+        }
+        1 -> when (col) {
+            0 -> return R.drawable.row_2_col_1
+            1 -> return R.drawable.row_2_col_2
+            2 -> return R.drawable.row_2_col_3
+            3 -> return R.drawable.row_2_col_4
+            else ->
+                return R.drawable.row_2_col_5
+        }
+        2 -> when (col) {
+            0 -> return R.drawable.row_3_col_1
+            1 -> return R.drawable.row_3_col_2
+            2 -> return R.drawable.row_3_col_3
+            3 -> return R.drawable.row_3_col_4
+            else ->
+                return R.drawable.row_3_col_5
+        }
+        3 -> when (col) {
+            0 -> return R.drawable.row_4_col_1
+            1 -> return R.drawable.row_4_col_2
+            2 -> return R.drawable.row_4_col_3
+            3 -> return R.drawable.row_4_col_4
+            else ->
+                return R.drawable.row_4_col_5
+        }
+        else ->
+            when (col) {
+                0 -> return R.drawable.row_5_col_1
+                1 -> return R.drawable.row_5_col_2
+                2 -> return R.drawable.row_5_col_3
+                3 -> return R.drawable.row_5_col_4
+                else ->
+                    return R.drawable.row_5_col_5
+            }
+
+    }
+
+}
+
 @Composable
-fun FlipCard(color:Color) {
+fun FlipCard(image: Unit) {
     var rotated by remember { mutableStateOf(false) }
     val rotation by animateFloatAsState(
         targetValue = if (rotated) 180f else 0f,
@@ -142,11 +192,13 @@ fun FlipCard(color:Color) {
         animationSpec = tween(100)
     )
     val animateColor by animateColorAsState(
-        targetValue = if (rotated) Color.Black else color,
+        targetValue = if(rotated) Color.Black else Color.Black.copy(alpha = 0f),
         animationSpec = tween(200)
     )
     Box(
-        Modifier.height(60.dp).width(60.dp),
+        Modifier
+            .height(60.dp)
+            .width(60.dp),
         contentAlignment = Alignment.Center
     ) {
         Box(
@@ -157,7 +209,8 @@ fun FlipCard(color:Color) {
                 }
                 .clickable {
                     rotated = !rotated
-                }.background(animateColor),
+                }
+                .background(animateColor),
         )
         {
             Column(
@@ -165,11 +218,17 @@ fun FlipCard(color:Color) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text(text = if (rotated) "" else "Front",
-                    modifier = Modifier
-                        .graphicsLayer {
-                            rotationY = rotation
-                        })
+                if(rotated)
+                {
+                    Text(text = "Back",
+                        modifier = Modifier
+                            .graphicsLayer {
+                                rotationY = rotation
+                            })
+                } else{
+                    image
+                }
+
             }
 
         }
