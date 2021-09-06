4ba88b48f21bf7a75e63baf629be60c0f8221f96
Daniel Sandberg <daniel.sandberg@elicit.se>
2021-09-06 20:21:03 +0200

    Flip image animation working

---
+++ LightsOutPuzzle.Jetpack/app/src/test/java/com/example/lightsoutpuzzlejetpack/GameTest.kt
@@ -0,0 +1,21 @@
+package com.example.lightsoutpuzzlejetpack
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class GameTest {
+
+    @Test
+    fun canStartGame() {
+        val game = Game(isFinished = false)
+        assertEquals(true, game.solveable())
+    }
+
+    @Test
+    fun canGetTile() {
+        val game = Game(isFinished = false)
+        val tile = game.getTile(2,3)
+        assertEquals(2, tile.row)
+        assertEquals(3, tile.col)
+    }
+}
